a346fe68cfaec33b97564adcdd54cf0a1ab8c345
Niranjan <niranjanthapa644122@gmail.com>
2024-02-09 08:03:38 +0545

    feat: validate the blocks in the chain

--- blockchain.js
+++ blockchain.js
@@ -11,4 +11,27 @@ class BlockChain {
         });
         this.chain.push(newBlock);
     }
+
+    static isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false
+        };
+        for (let i = 1; i < chain.length; i++) {
+            const { timeStamp, prevHash, hash, data } = chain[i];
+            const realLastHash = chain[i - 1].hash;
+
+            if (prevHash !== realLastHash) {
+                return false;
+            }
+
+            const validatedHash = cryptoHash(timeStamp, prevHash, data);
+
+            if (hash !== validatedHash) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+module.exports = BlockChain;
